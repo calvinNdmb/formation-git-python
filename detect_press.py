19d57433f32c0edef0336d9f4acc647ee5192afa
calvinndmb <calvin.ndoumbe@efrei.net>
2026-03-23 14:12:43 +0100

    Add functions to handle snake movement and key press detection

---
+++ detect_press.py
@@ -0,0 +1,2 @@
+def detect_press():  # Détecter l'appui d'une touche
+    return input("Direction (up/down/left/right) : ")
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 
 
-def newCoords(x,y,input):# Déterminer les nouvelles coordonées
 
-    return (x,y)
-    pass
-
-def detect_press():# Détecter l'appuie d'une touche
-    return input
-    pass       
-
+def draw_snake(x, y, width=10, height=10):
+    for j in range(height):
+        for i in range(width):
+            if i == x and j == y:
+                print("*", end="")
+            else:
+                print(".", end="")
+        print()
 
 def main():
-    pass
+    snake_coords = [9, 10]
+    print("#" * 10)
+    draw_snake(snake_coords[0], snake_coords[1])
+    print("#" * 10)
+
+main()
