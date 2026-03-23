d0e7394bfaa39cc580ee1f23b24b83adb59ce04f
calvinndmb <calvin.ndoumbe@efrei.net>
2026-03-23 13:46:55 +0100

    mainfunction

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def newCoords(x,y,input):# Déterminer les nouvelles coordonées
+
+    return (x,y)
+    pass
+
+def detect_press():# Détecter l'appuie d'une touche
+    return input
+    pass       
+
+
+def main():
+    pass
